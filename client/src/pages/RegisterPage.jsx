f3bc0ce6169f5c94d7fbc6547652db9987e25c05
Sirapop Chanphui <srp073536@gmail.com>
2026-02-06 15:15:02 +0700

    feat: implement authentication with JWT and user registration/login functionality

--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -1,4 +1,7 @@
 import { useState } from "react";
+import { useAuth } from "../contexts/authentication";
+
+
 
 function RegisterPage() {
   const [username, setUsername] = useState("");
@@ -6,10 +9,19 @@ function RegisterPage() {
   const [lastName, setLastName] = useState("");
   const [password, setPassword] = useState("");
 
+  const { register } = useAuth();
+
   const handleSubmit = (event) => {
     event.preventDefault();
     // 🐨 Todo: Exercise #2
     // นำ Function `register` ใน AuthContext มา Execute ใน Event Handler ตรงนี้
+    const data = {
+      username,
+      password,
+      firstName,
+      lastName,
+    };
+    register(data);
   };
 
   return (
--- server/apps/auth.js
+++ server/apps/auth.js
@@ -1,11 +1,69 @@
 import { Router } from "express";
+import { db } from "../utils/db.js"
+import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken";
 
 const authRouter = Router();
 
 // 🐨 Todo: Exercise #1
 // ให้สร้าง API เพื่อเอาไว้ Register ตัว User แล้วเก็บข้อมูลไว้ใน Database ตามตารางที่ออกแบบไว้
+authRouter.post("/register", async (req, res) => {
+  const user = {
+    username: req.body.username,
+    password: req.body.password,
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  user.password = await bcrypt.hash(user.password, salt);
+
+  const collection = db.collection("users");
+  await collection.insertOne(user);
+
+  return res.json({
+    messsage: "User hasbeen created successfully"
+  })
+})
 
 // 🐨 Todo: Exercise #3
 // ให้สร้าง API เพื่อเอาไว้ Login ตัว User ตามตารางที่ออกแบบไว้
+
+
+authRouter.post("/login", async (req, res) => {
+
+  const user = await db.collection("users").findOne({
+    username: req.body.username
+  });
+
+  if (!user) {
+    return res.status(401).json({
+      "message": "Invalid username or password"
+    });
+  }
+
+  const isValidPassword = await bcrypt.compare(req.body.password, user.password);
+
+  if (!isValidPassword) {
+    return res.status(401).json({
+      "message": "Invalid username or password"
+    });
+  }
+
+  const token = jwt.sign(
+    {
+      id: user.id,
+      firstName: user.firstName,
+      lastName: user.lastName
+    },
+    process.env.SECRET_KEY,
+    {
+      expiresIn: '900000',
+    }
+  );
+
+  return res.json({ message: "login succesfully", token })
+
+});
 
 export default authRouter;
